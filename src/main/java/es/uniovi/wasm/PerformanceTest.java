0f0c2ea2f69419c05e1cf39bd0c4b3c982a054af
Luis Emilio Velasco Sanchez <emibloque@gmail.com>
2021-05-11 21:03:32 +0200

    Add more tests

--- src/main/java/es/uniovi/wasm/PerformanceTest.java
+++ src/main/java/es/uniovi/wasm/PerformanceTest.java
@@ -1,17 +1,45 @@
 package es.uniovi.wasm;
 
+import java.util.HashMap;
+
 class PerformanceTest {
+
+    public static int multiplyInt(int n) {
+        int c = 1;
+        for (var i = 0; i < n; i++) {
+            c = c * 1 * 1;
+        }
+        return c;
+    }
 
     public static int fib(int n) {
         if (n == 1 || n == 2) return 1;
         return fib(n - 1) + fib(n - 2);
     }
 
-    public static void run(String env) {
+    public static double benchmark(Runnable fn) {
+        int loop = 10;
         long startTime = System.currentTimeMillis();
-        System.out.println(env + ": Fib: " + fib(40));
+        for (int i = 0; i < loop; i++) {
+            fn.run();
+        }
         long endTime = System.currentTimeMillis();
-        System.out.println(env + ": Time: " + (endTime - startTime));
+        return ((endTime - startTime) / 1. / loop);
+    }
+
+    public static void run(String env) {
+        HashMap<String, Runnable> funcs = new HashMap<>() {{
+            put("Fibonacci", () -> fib(40));
+            put("MultiplyInt", () -> multiplyInt(268435456));
+        }};
+
+        System.out.println(env + ": Start");
+
+        for (var entry : funcs.entrySet()) {
+            System.out.println(env + ": Time [" + entry.getKey() + "]: " + benchmark(entry.getValue()));
+        }
+
+        System.out.println(env + ": Done");
     }
 }
 
